5edf5cc43a0671db145b20a4ee6e792ba190c437
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-24 01:03:39 +0500

    Working with Ses

--- seswithpython.py
+++ seswithpython.py
@@ -2,22 +2,31 @@ import boto3
 
 ses =boto3.client('ses', region_name='eu-north-1')
 response=ses.list_verified_email_addresses()
+print(response)
 
-'''response2 = ses.create_template(
+
+#Create Template for Email
+def createtemplate():
+ response2 = ses.create_template(
     Template={
         'TemplateName': 'string3',
         'SubjectPart': 'string3',
         'TextPart': 'string3',
         'HtmlPart': 'string3'
-    }
-)'''
+    })
+ print(response2)
+
 
 #Listing ALl Templates
-response=ses.list_templates()
-print(response)
+def list_templates():
+ response=ses.list_templates()
+ print(response)
+
+
+def sendemail():
 
 #Sending Email
-response22=ses.send_email(
+ response22=ses.send_email(
     Source='as1987137@gmail.com',
     Destination={
         'ToAddresses': [
@@ -25,10 +34,7 @@ response22=ses.send_email(
         ],
         'CcAddresses': [
             'msami.bese22seecs@seecs.edu.pk',
-        ]'Html': {
-                'Data': 'string',
-                'Charset': 'string'
-            }
+        ],
     },
     Message={
         'Subject': {
@@ -41,5 +47,36 @@ response22=ses.send_email(
                 'Charset': 'utf-8'
             }
         }
-    }
-)
+    })
+
+def update_template():
+ template_name = 'string3'
+
+ updated_subject = 'Testing Email'
+ updated_text_body = 'This is the updated text body of the email.'
+
+
+ response2 = ses.update_template(
+    Template={
+        'TemplateName': template_name,
+        'SubjectPart': updated_subject,
+        'TextPart': updated_text_body,
+
+    })
+ print(response2)
+def sendtemplatedemail():
+ template_name = 'string3'
+ response = ses.send_templated_email(
+    Source='as1987137@gmail.com',
+    Destination={
+        'ToAddresses': [
+            'msami.bese22seecs@seecs.edu.pk',
+        ],
+        'CcAddresses': [
+            'msami.bese22seecs@seecs.edu.pk',
+        ],
+    },
+    Template=template_name,  # Use the template name
+    TemplateData='{}',  # You can provide template data if your template expects variables
+      )
+ print(response)
