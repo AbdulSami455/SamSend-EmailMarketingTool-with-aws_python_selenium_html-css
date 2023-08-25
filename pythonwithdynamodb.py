7449da678c2243f26ae56832477fb9309dec53ac
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-25 23:34:18 +0500

    Working with Dynamo+Templates

--- pythonwithdynamodb.py
+++ pythonwithdynamodb.py
@@ -2,7 +2,7 @@ import boto3
 
 dynamodb=boto3.client('dynamodb', region_name='eu-north-1')
 
-
+#Create Table on DynamoDb
 def createtable(tablename):
 
  table_name = tablename
@@ -28,6 +28,8 @@ def createtable(tablename):
 
  print(dynamodb)
 
+
+#Enter Data in DynamoDb
 def enterData(tablename,templatename,subject,body,html):
  dynamodb.get_waiter('table_exists').wait(TableName=tablename)
 
@@ -52,3 +54,35 @@ def enterData(tablename,templatename,subject,body,html):
  print('Data inserted into the table.')
 
 
+#Fetch Template Data
+def fetch_template_data(tablename, templatename):
+    response = dynamodb.get_item(
+        TableName=tablename,
+        Key={
+            'TemplateName': {'S': templatename}
+        },
+        ProjectionExpression='Subject, Body, Html'
+    )
+
+    item = response.get('Item')
+    if item:
+        return {
+            'Subject': item['Subject']['S'],
+            'Body': item['Body']['S'],
+
+        }
+    else:
+        return None
+
+
+table_name = 'EmailTemplates'
+template_name = 'Template2'
+
+template_data = fetch_template_data(table_name, template_name)
+
+if template_data:
+    print("Subject:", template_data['Subject'])
+    print("Body:", template_data['Body'])
+
+else:
+    print("Template not found.")
