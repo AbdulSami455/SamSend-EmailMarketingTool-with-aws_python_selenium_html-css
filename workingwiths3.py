5c6355c96edbbf7e5dd46741e51b49facb4704de
sami <msami.bese22seecs@student.nust.edu.pk>
2023-08-28 21:33:15 +0500

    s3 working

--- workingwiths3.py
+++ workingwiths3.py
@@ -2,4 +2,15 @@ import boto3
 
 s3=boto3.client('s3', region_name='eu-north-1')
 
+response = s3.get_object(Bucket='emailtemplates007', Key='eventinvitation.txt')
 
+# Read the content of the object
+object_content = response['Body'].read().decode('utf-8')
+
+# Print or manipulate the content as needed
+print(object_content)
+subject, body = object_content.split("===")
+
+# Print the subject and body
+print("Subject:", subject.strip())  # Remove leading/trailing whitespace
+print("\nBody:", body.strip())      # Remove leading/trailing whitespace
