6cc7896c49ac4ad100f2e9a2fc432aa3d35427fd
sami <msami.bese22seecs@student.nust.edu.pk>
2023-09-13 20:55:50 +0500

    Styling

--- main.py
+++ main.py
@@ -11,12 +11,19 @@ def mainpage():
 app.run()
 '''
 import tkinter as tk
-from tkinter import PhotoImage
-import os
+
 # Create the main window
 root = tk.Tk()
 root.title("SamSend Email Marketing Tool")
-
 root.geometry("1000x600")
 
+# Create a frame
+composeemail = tk.Frame(master=root)
+composeemail.place(x=25, y=80, relwidth=0.6, relheight=0.4)
+
+# Add a label to the frame
+label = tk.Label(composeemail, text="This is a label inside the frame")
+label.pack()  # Pack the label to make it visible inside the frame
+
 root.mainloop()
+
