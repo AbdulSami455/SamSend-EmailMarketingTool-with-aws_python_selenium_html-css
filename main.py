039b89c1fb71713d880148bbb05fbbb128683092
sami <msami.bese22seecs@student.nust.edu.pk>
2023-09-13 21:21:30 +0500

    Styling

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def mainpage():
 app.run()
 '''
 import tkinter as tk
+from PIL import ImageTk, Image
 
 # Create the main window
 root = tk.Tk()
@@ -18,12 +19,35 @@ root.title("SamSend Email Marketing Tool")
 root.geometry("1000x600")
 
 # Create a frame
-composeemail = tk.Frame(master=root)
-composeemail.place(x=25, y=80, relwidth=0.6, relheight=0.4)
+composeemail = tk.Frame(master=root,bg="lightblue")
+composeemail.place(x=70, y=180, relwidth=0.2, relheight=0.4)
 
-# Add a label to the frame
-label = tk.Label(composeemail, text="This is a label inside the frame")
-label.pack()  # Pack the label to make it visible inside the frame
+writetemplate=tk.Frame(master=root,bg="lightblue")
+writetemplate.place(x=380,y=180,relwidth=0.2,relheight=0.4)
+
+list=tk.Frame(master=root,bg="lightblue")
+list.place(x=690,y=180,relwidth=0.2,relheight=0.4)
+
+image = Image.open("list.png")
+image = image.resize((200, 240), Image.ANTIALIAS)
+image_tk = ImageTk.PhotoImage(image)
+label = tk.Label(list, image=image_tk)
+label.configure(text="")
+label.pack()
+
+image = Image.open("compose-mail-2.png")
+image = image.resize((200, 240), Image.ANTIALIAS)
+image_tk2= ImageTk.PhotoImage(image)
+label2 = tk.Label(composeemail, image=image_tk2)
+label2.configure(text="")
+label2.pack()
+
+image = Image.open("templates.png")
+image = image.resize((200, 240), Image.ANTIALIAS)
+image_tk3= ImageTk.PhotoImage(image)
+label3 = tk.Label(writetemplate, image=image_tk3)
+label3.configure(text="")
+label3.pack()
 
 root.mainloop()
 
