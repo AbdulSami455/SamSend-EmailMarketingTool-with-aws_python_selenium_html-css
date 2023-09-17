6b888943fdb3682f92175008d5f8236cc5db5d6b
sami <msami.bese22seecs@student.nust.edu.pk>
2023-09-17 20:21:54 +0500

    email

--- writetemplates.py
+++ writetemplates.py
@@ -4,5 +4,10 @@ import seswithpython as sp
 def write():
     window=tk.Tk()
     window.geometry("800x600")
+    templates = sp.list_templates()
+    print(templates['names'])
+
+    # Insert template names into the text box
+
+
     window.mainloop()
-    sp.list_templates()
